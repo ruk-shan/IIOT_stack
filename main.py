2519fa136f337c7c7ab700938649d94d5771fc0e
ruk.shan <ruk.shan@outlook.com>
2021-12-06 00:05:19 +0800

    added config

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ from config import read_config
 
 @app.get("/")
 def read_root():
+    """ Info """
     return {"IIOT_stack":"alhpa-v0.1","Author":"Rukshan H", "Email":"rukshan_h@artc.a-star.edu.sg"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/read_config")
+def read_read_config():
+    """Read config.json file """
+    return read_config()
+
+
+
 
 
 if __name__ == "__main__":
