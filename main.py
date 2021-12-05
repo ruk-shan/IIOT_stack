2f17b4532e6fe528664ab83d8b6092352faac0e6
ruk.shan <ruk.shan@outlook.com>
2021-12-05 15:57:11 +0800

    IIOT_stack_alhpa-v0.1-init commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from typing import Optional
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"IIOT_stack":"alhpa-v0.1","Author":"Rukshan H", "Email":"rukshan_h@artc.a-star.edu.sg"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8001)
